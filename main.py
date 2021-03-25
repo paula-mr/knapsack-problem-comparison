7ff6de45e6ea839b1496fbdfc15ef58cefa5c718
Paula Ribeiro <paula.mribeiro05@gmail.com>
2021-03-25 01:03:06 -0300

    implement branch and bound

--- main.py
+++ main.py
@@ -43,15 +43,69 @@ def recursive_backtracking(max_profit, current_profit, available_weight, items):
     
     return max_profit
 
+
+def bound(node, n, weight, items):
+    level, profit, current_weight = node
+    if current_weight > weight:
+        return 0
+
+    profit_bound = profit
+    j = level + 1
+    total_weight = current_weight
+
+    while j < n and total_weight + items[j][1] <= weight:
+        profit_bound += items[j][0]
+        total_weight += items[j][1]
+        j += 1
     
+    print('a', total_weight, profit_bound)
+    if j < n:
+        profit_bound += (weight - current_weight) * items[j][0]/items[j][1]
+    
+    return profit_bound
 
 
 def solve_knapsack_branch_and_bound(weight, items):
     items = sort_array(items)
+    n = len(items)
     max_profit = 0
-    queue = [(0,0)]
+    root = (-1, 0, 0) # level, profit, weight
+    queue = [root]
 
-    return 0
+    while queue:
+        node = queue[0]
+        queue = queue[1:]
+
+        node_level = node[0]
+        node_profit = node[1]
+        node_weight = node[2]
+
+        if node_level == n-1:
+            continue
+        
+        new_node_level = node_level+1
+        new_node_profit = node_profit+items[node_level+1][0]
+        new_node_weight = node_weight+items[node_level+1][1]
+
+        # taking the item
+        if new_node_weight <= weight and new_node_profit > max_profit:
+            max_profit = new_node_profit
+        new_node_bound = bound((new_node_level, new_node_profit, new_node_weight), n, weight, items)
+
+        print (new_node_bound, max_profit)
+        if new_node_bound > max_profit:
+            queue = [(new_node_level, new_node_profit, new_node_weight)] + queue
+        
+        # without taking the item        
+        new_node_weight = node_weight
+        new_node_profit = node_profit
+        new_node_bound = bound((new_node_level, new_node_profit, new_node_weight), n, weight, items)
+
+        print (new_node_bound, max_profit)
+        if new_node_bound > max_profit:
+            queue = [(new_node_level, new_node_profit, new_node_weight)] + queue
+
+    return max_profit
 
 def compute_ratio(item):
     return item[0]/item[1]
@@ -82,7 +136,7 @@ def get_startup_arguments(argv):
         algorithm = argv[1]
     else:
         print('Invalid arguments')
-        print("main.py <INPUT FILE> <ALGORITHM = bt | b&b>")
+        print("main.py <INPUT FILE> <ALGORITHM = bt | bandb>")
         os._exit(1)
     
     return input_file, algorithm
